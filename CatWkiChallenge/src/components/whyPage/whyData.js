193e77b702e277fb3c2439d8a1f1fdf0f3cfd63d
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2023-12-05 17:12:22 -0300

    Se termina con la página de 'why section'

---
+++ CatWkiChallenge/src/components/whyPage/whyData.js
@@ -0,0 +1,50 @@
+export const whyData = [
+  {
+    id: "stressReduction",
+    benefitName: "Stress Reduction",
+    benefitContent:
+      "Cats are natural stress relievers. Their presence triggers the release of calming chemicals in your body, lowering stress and anxiety levels.",
+  },
+  {
+    id: "improvedMentalHealth",
+    benefitName: "Improved Mental Health",
+    benefitContent:
+      "Interacting with cats increases the production of endorphins, improving overall mental well-being and emotional stability.",
+  },
+  {
+    id: "companionshipAndRoutine",
+    benefitName: "Companionship and Routine",
+    benefitContent:
+      "Caring for a cat provides a sense of purpose and structure in daily life. Their needs offer companionship and fulfillment.",
+  },
+  {
+    id: "potentialHeartHealthBenefits",
+    benefitName: "Potential Heart Health Benefits",
+    benefitContent:
+      "Having a cat may reduce the risk of heart disease by lowering blood pressure and decreasing the production of cortisol.",
+  },
+  {
+    id: "socialSupportAndReducedLoneliness",
+    benefitName: "Social Support and Reduced Loneliness",
+    benefitContent:
+      "Cats serve as companions, offering social support and reducing feelings of loneliness. Their presence provides emotional comfort.",
+  },
+  {
+    id: "therapeuticEffectOnChildren",
+    benefitName: "Therapeutic Effect on Children",
+    benefitContent:
+      "Growing up with a cat can help children develop empathy, responsibility, and reduce the risk of allergies and asthma.",
+  },
+  {
+    id: "sourceOfEntertainment",
+    benefitName: "Source of Entertainment",
+    benefitContent:
+      "Cats' playful behavior and antics provide entertainment, joy, and laughter in the home.",
+  },
+  {
+    id: "improvedImmuneSystem",
+    benefitName: "Improved Immune System",
+    benefitContent:
+      "Exposure to cats at a young age may bolster the immune system and reduce the risk of allergies and respiratory issues later in life.",
+  },
+];
